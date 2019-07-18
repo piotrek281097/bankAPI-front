450a426baae01b549e4e2df0d78c615bc0d0d3e9
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-07-18 16:33:29 +0200

    dodanie angularjson, packagejson,proxyconfigjson

--- bankAPI-front/server.js
+++ bankAPI-front/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/bankcomarchrestapi'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/bankcomarchrestapi/index.html'));
 });
 
-app.listen(8081 || 4000, function(){
-    console.log('Your node js server is running');
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8081);
